d31ed46a7e7267726c719e96c86ee0c44c2e10b7
Laxman7289 <127031359+Laxman7289@users.noreply.github.com>
2023-03-09 12:48:17 +0530

    First commit

---
+++ src/component/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className='container'>
+        <div className='header'>
+            LoGo
+        </div>
+    </div>
+  )
+}
