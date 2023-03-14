52b7dd144b613656d175d78506876a1500c2b9fa
Laxman7289 <127031359+Laxman7289@users.noreply.github.com>
2023-03-14 13:14:54 +0530

    footer add

--- src/component/Details.js
+++ src/component/Details.js
@@ -4,31 +4,91 @@ import css from "./Details.module.css"
 
 
 const Details = () => {
-  return (
-    <div className={css.data}>
-        <div className={css.data_heading}>
-            <h5>Build amazing </h5>
-            <h5>things</h5>
-        </div>
-        <div className={css.data_content}>
-            <p>
-            We're GitHub., the company behind the npm Registry and npm CLI. We offer those to the community for free, but our day job is building and selling useful tools for developers like you.
-            </p>
-            <h4>Take your JavaScript</h4>
-            <h4> development up a notch</h4>
-            <p>Get started today for free, or step up to npm Pro to enjoy a premium JavaScript development experience, with features like private packages.</p>
-        </div>
-        <div className={css.btn}>
-            <Button variant="text" color="primary" className={css.btn_one}>
-                sign up for free
-            </Button>
-            <Button variant="text" color="primary" className={css.btn_second} >
-                Learn about Pro
-            </Button>
-        </div>
-      
-    </div>
-  )
+    return (
+        <>
+            <div className={css.data}>
+                <div className={css.data_heading}>
+                    <h5>Build amazing </h5>
+                    <h5>things</h5>
+                </div>
+                <div className={css.data_content}>
+                    <p>
+                        We're GitHub., the company behind the npm Registry and npm CLI.
+                        We offer those to the community for free, but our day job is building and selling
+                        useful tools for developers like you.
+                    </p>
+                    <h4>Take your JavaScript</h4>
+                    <h4> development up a notch</h4>
+                    <p>Get started today for free, or step up to npm Pro to enjoy a premium JavaScript development experience, with features like private packages.</p>
+                </div>
+                <div className={css.btn}>
+                    <Button variant="text" color="primary" className={css.btn_one}>
+                        sign up for free
+                    </Button>
+                    <Button variant="text" color="primary" className={css.btn_second} >
+                        Learn about Pro
+                    </Button>
+                </div>
+            </div>
+            <div className={css.iconimg}>
+                <img src='https://static.npmjs.com/attachments/ck3uweazy72ye8874y9kkxnx1-gak.png' />
+            </div>
+            <div className={css.heading2}>
+                <h2>Bring the best of open</h2>
+                <h2> source to you, your team,</h2>
+                <h2>and your company</h2>
+            </div>
+            <div className={css.details2}>
+                <p>
+                    Relied upon by more than 17 million developers worldwide, npm is<br></br>
+                    committed to making JavaScript development elegant, productive, and<br></br>
+                    safe. The free npm Registry has become the center of JavaScript code<br></br>
+                    sharing, and with more than two million packages, the largest software<br></br>
+                    registry in the world. Our other tools and services take the Registry, and<br></br>
+                    the work you do around it, to the next level.
+                </p>
+            </div>
+
+            {/* <div className={css.icons}>
+                <img className={css.icons_one} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_f_J4xh7bHZyMHcS02_GK695rB6nDQSffcg&usqp=CAU'/>
+                <img className={css.icons_second} src='https://w7.pngwing.com/pngs/914/758/png-transparent-github-social-media-computer-icons-logo-android-github-logo-computer-wallpaper-banner-thumbnail.png'/>
+            </div> */}
+            <div className={css.footer}>
+                <div className={css.footer_0}>
+                    
+                </div>
+                <div className={css.footer_box1}>
+                    <p className={css.footer_botom}>Support</p>
+                    <ul>
+                        <li>Help</li>
+                        <li>Advisories</li>
+                        <li>Status</li>
+                        <li>Contact npm</li>
+                    </ul>
+                </div>
+
+                <div className={css.footer_box2}>
+                    <p className={css.footer_botom}>company</p>
+                    <ul>
+                        <li>About</li>
+                        <li>Blog</li>
+                        <li>Press</li>
+                    </ul>
+                </div>
+
+                <div className={css.footer_box3}>
+                    <p className={css.footer_botom}>Terms & Policies</p>
+                    <ul>
+                        <li>Policies</li>
+                        <li>Terms of use</li>
+                        <li>Code of conduct</li>
+                        <li>Privacy</li>
+                    </ul>
+                </div>
+
+            </div>
+        </>
+    )
 }
 
 export default Details
